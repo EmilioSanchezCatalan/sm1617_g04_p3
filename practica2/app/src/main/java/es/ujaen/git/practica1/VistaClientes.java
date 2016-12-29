7d2f88e19606edb1a428e1427e44d68a5e24d858
vmpc0002 <vmpc0002@red.ujaen.es>
2016-12-29 20:43:01 +0100

    Mantener datos ante cambio de estado en proceso

--- practica2/app/src/main/java/es/ujaen/git/practica1/VistaClientes.java
+++ practica2/app/src/main/java/es/ujaen/git/practica1/VistaClientes.java
@@ -11,6 +11,7 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -24,6 +25,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -37,30 +39,32 @@ import java.util.List;
 public class VistaClientes extends AppCompatActivity implements Service {
 
     ListView listView;
-    ArrayList<Producto> productos;
+    ArrayList<Producto> productos = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vista_clientes);
         listView = (ListView) findViewById(R.id.vistaclientes_listamenu_listview);
-        Handler handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                productos = new ArrayList();
-                Bundle datos = msg.getData();
-                InputStream stream = new ByteArrayInputStream(datos.getString("key").getBytes(StandardCharsets.UTF_8));
-                JsonMenuParser parser = new JsonMenuParser();
-                try {
-                    productos = parser.readJsonStream(stream);
-                } catch (IOException ex) {
+        if (savedInstanceState == null) {
+            Handler handler = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    Bundle datos = msg.getData();
+                    InputStream stream = new ByteArrayInputStream(datos.getString("key").getBytes(StandardCharsets.UTF_8));
+                    JsonMenuParser parser = new JsonMenuParser();
+                    try {
+                        productos = parser.readJsonStream(stream);
+                    } catch (IOException ex) {
 
+                    }
+                    CargaImagen load = new CargaImagen();
+                    load.execute();
                 }
-                CargaImagen load = new CargaImagen();
-                load.execute();
-            }
-        };
-        new Thread(new Listar(handler)).start();
+            };
+            new Thread(new Listar(handler)).start();
+
+        }
     }
 
     @Override
@@ -80,7 +84,7 @@ public class VistaClientes extends AppCompatActivity implements Service {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        switch (id){
+        switch (id) {
             case R.id.configuration:
                 SharedPreferences pref = getSharedPreferences(sharedpreferences, 0);
                 SharedPreferences.Editor editorPref = pref.edit();
@@ -94,6 +98,35 @@ public class VistaClientes extends AppCompatActivity implements Service {
         }
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("size", productos.size());
+        for (int i = 0; i < productos.size(); i++) {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            productos.get(i).getImagen().compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            outState.putString("nombre" + i, productos.get(i).getNombre());
+            outState.putByteArray("imagen" + i, byteArray);
+            outState.putDouble("precio" + i, productos.get(i).getPrecio());
+            outState.putString("descripcion" + i, productos.get(i).getDescripcion());
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        for (int i = 0; i < savedInstanceState.getInt("size"); i++) {
+            byte[] byteArray = savedInstanceState.getByteArray("imagen" + i);
+            productos.get(i).setNombre(savedInstanceState.getString("nombre" + i));
+            productos.get(i).setImagen(BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length));
+            productos.get(i).setPrecio(savedInstanceState.getDouble("precio"+i));
+            productos.get(i).setDescripcion(savedInstanceState.getString("descripcion"+i));
+        }
+        AdapterMenuList adapter = new AdapterMenuList(VistaClientes.this, productos);
+        listView.setAdapter(adapter);
+    }
+
     class CargaImagen extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
@@ -101,7 +134,7 @@ public class VistaClientes extends AppCompatActivity implements Service {
             bmOptions = new BitmapFactory.Options();
             bmOptions.inSampleSize = 1;
             for (int i = 0; i < productos.size(); i++) {
-                productos.get(i).setImagen(LoadImagen.loadBitmap("http://"+servidor+":"+port+"/"+servicio
+                productos.get(i).setImagen(LoadImagen.loadBitmap("http://" + servidor + ":" + port + "/" + servicio
                         + productos.get(i).getSimagen(), bmOptions));
             }
             return null;
